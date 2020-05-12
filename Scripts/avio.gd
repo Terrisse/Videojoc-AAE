9e410911cb52a2bea6d98e6adbf667f8dbdb931b
ainalopsann <60349571+ainalopsann@users.noreply.github.com>
2020-05-12 20:44:57 +0200

    Escena avió
    He creat el primer avió que ja es pot moure up/down. S'ha de moure el fons horitzontalment.

---
+++ Scripts/avio.gd
@@ -0,0 +1,36 @@
+extends Area2D
+var velocitat=200
+var moviment=Vector2()
+# per què no es pugui sortir de la pantalla:
+var pantalla
+
+
+func _ready():
+	#hide() #amago a fatboy perquè surti el menú inicial
+	pantalla=get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	mou(delta)
+	anima()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func mou(delta):
+	if Input.is_action_pressed("adalt"):
+		moviment.y-=1
+	elif Input.is_action_pressed("abaix"):
+		moviment.y+=1
+	else:
+		moviment=Vector2.ZERO
+	
+	#hem de normalitzar la velocitat ja que en el cas que vagi en diagonal anirà més ràpid 
+	if moviment.length()>0: #en aquest cas s'estarà movent
+		moviment=moviment.normalized()*velocitat
+	position+=moviment * delta
+	position.y=clamp(position.y,10,pantalla.y-100)
+	
+func anima():
+	pass
