8b9d38964058f8bddc4f27b6a34f73897e9437c4
ainalopsann <60349571+ainalopsann@users.noreply.github.com>
2020-05-16 14:04:31 +0200

    bala
    creació bala

--- Scripts/avio.gd
+++ Scripts/avio.gd
@@ -3,6 +3,13 @@ var velocitat=200
 var moviment=Vector2()
 # per què no es pugui sortir de la pantalla:
 var pantalla
+onready var Posicio_avio=$Posicio_avio
+onready var Timer_temps_bala=$Timer_temps_bala
+var bala= load("res://Scenes/bala.tscn")
+var direccio_bala=Vector2()
+
+
+
 
 
 func _ready():
@@ -14,11 +21,21 @@ func _ready():
 func _process(delta):
 	mou(delta)
 	anima()
+func disparar(bala) -> void:
+	if !self.Timer_temps_bala.is_stopped():
+		return 
+	var nova_bala=bala.instance()
+	self.get_parent().add_child(nova_bala)
+	nova_bala.global_position=self.Posicio_avio.global_position
+	self.Timer_temps_bala.start()
 	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func mou(delta):
+	
+	if Input.is_action_pressed("disparar"):
+		disparar(bala)
+		
 	if Input.is_action_pressed("adalt"):
 		moviment.y-=1
 	elif Input.is_action_pressed("abaix"):
@@ -30,7 +47,7 @@ func mou(delta):
 	if moviment.length()>0: #en aquest cas s'estarà movent
 		moviment=moviment.normalized()*velocitat
 	position+=moviment * delta
-	position.y=clamp(position.y,10,pantalla.y-100)
+	#position.y=clamp(position.y,10,pantalla.y-100)
 	
 func anima():
 	pass
---
+++ Scripts/bala.gd
@@ -0,0 +1,29 @@
+extends Area2D
+var velocitat_bala=200
+var moviment_bala=Vector2()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	mou_bala(delta)
+	
+func mou_bala(delta):
+	#hem de normalitzar la velocitat ja que en el cas que vagi en diagonal anirà més ràpid 
+	moviment_bala.x+=1
+	moviment_bala=moviment_bala.normalized()*velocitat_bala
+	position+=moviment_bala * delta
+	
+
+func _on_Timer_eliminar_bala_timeout():
+	
+
+	queue_free()
