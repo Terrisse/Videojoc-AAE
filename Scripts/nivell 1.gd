91bbc3d78a68878eeaa576b35187fe1fce695da4
Terrisse <60349439+Terrisse@users.noreply.github.com>
2020-05-25 11:58:28 +0200

    Create nivell 1.gd

---
+++ Scripts/nivell 1.gd
@@ -0,0 +1,60 @@
+extends Node
+export (PackedScene) var enemic1
+var inici_nivell_1 =false
+
+
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer_inici_joc.start()
+	randomize()
+	
+	
+	
+func game_over_nivell_1():
+	get_tree().change_scene("res://Scenes/menus/has perdut.tscn")
+	
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
+
+
+func _on_Timer_inici_joc_timeout():
+	$Timer_nou_enemic.start()
+	
+
+
+func _on_Timer_nou_enemic_timeout():
+	
+	var E = enemic1.instance()
+	E.position.x=$avio.position.x + 1000
+	E.position.y= randi() % 800
+	
+	
+	#E.position.x+=1000
+	add_child(E) # es crea un nou enemic
+	print('creacioenemic')
+	
+	
+func _on_avio_explosio_collision():
+	print("ha colisionat")
+	$avio/AnimatedSprite.hide()
+	game_over_nivell_1()
+	
+	
+
+
+
+func _on_avio_collisio_amb_enemic1():
+	game_over_nivell_1() 
+	pass 
